1d3979a99e39deebf99b1a92cb8947e7a2f8974c
Sanjit Kumar Mishra <sanjitforinia@gmai.com>
2013-07-11 20:18:02 +0530

    update web.js file

--- web.js
+++ web.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+ var htmlfile = new Buffer(24);
+ var text = fs.readFileSync('index.html', 'utf8');
+ htmlfile.write(text, "utf-8");
   response.send('Hello World2!');
 });
 
